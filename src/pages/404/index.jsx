5d64da6f5e83c95bcbafd27ca2e2a0f74312591a
lwojski <lilian.woj@icloud.com>
2024-08-19 19:26:12 +0200

    Responsive de la page erreur 404

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -8,7 +8,8 @@ function NotFound404() {
       <div className="page404Content">
         <h1 className="page404Title">404</h1>
         <p className="page404Message">
-          Oups! La page que vous demandez n'existe pas.
+          <span className="404MessageLine1">Oups! La page que </span>
+          <span className="404MessageLine2">vous demandez n'existe pas.</span>
         </p>
         <Link to="/" className="page404Link">
           Retourner sur la page d'accueil
